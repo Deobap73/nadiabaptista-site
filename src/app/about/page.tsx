b75a43a6afb6c6fc1f6f4a3e0492521cacaafb54
Deolindo Baptista <contact@deolindobaptista.com>
2025-12-13 16:55:35 +0100

    feat(about): add motivation sentence section"  Changes to be committed: 	modified:   src/app/about/page.tsx 	new file:   src/components/about/AboutMotivationSentence.tsx 	modified:   src/styles/components/_about.scss

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,17 +1,13 @@
 // src\app\about\page.tsx
 
+import AboutMotivationSentence from '@/components/about/AboutMotivationSentence';
 import AboutHero from '../../components/about/AboutHero';
 
 export default function AboutPage() {
   return (
     <>
       <AboutHero />
-      <section className='page_section'>
-        <div className='page_section__inner'>
-          <h2>Sobre mim</h2>
-          <p>Conteudo a completar na proxima fase.</p>
-        </div>
-      </section>
+      <AboutMotivationSentence />
     </>
   );
 }
---
+++ src/components/about/AboutMotivationSentence.tsx
@@ -0,0 +1,11 @@
+// src/components/about/AboutMotivationSentence.tsx
+
+export default function AboutMotivationSentence() {
+  return (
+    <section className='about_motivation' aria-labelledby='aboutMotivationHeading'>
+      <p id='aboutMotivationHeading' className='about_motivation__text'>
+        Conhecimento é poder, mas conhecimento sobre si mesmo é auto capacitação
+      </p>
+    </section>
+  );
+}
